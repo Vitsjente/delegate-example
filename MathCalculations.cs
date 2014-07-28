4b56c5eb17a414a9bc6e36855eb3ea4223ed4c1b
Vincent <vincent@arbox.nl>
2014-07-28 21:53:54 +0200

    Initial commit
    Test project to experiment with delegates in c#

---
+++ MathCalculations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_example
+{
+    public class MathCalculations
+    {
+        //Create delegate for all math calculation
+        public delegate void Calculation(int i, int j);
+
+        //Handle multiple delegate actions
+        public Calculation Calculations;
+
+        public Calculation MultiplyCalculation;
+
+        //Create multiple 
+        public MathCalculations()
+        {
+            Calculations = Add;
+            Calculations += Multiply;
+            Calculations += Divide;
+
+            MultiplyCalculation = Multiply;
+        }
+
+        /// <summary>
+        /// Add two integers and write them to the screen
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        public void Add(int i, int j)
+        {
+            Console.WriteLine(String.Format("Add result {0}", i + j));
+        }
+
+        /// <summary>
+        /// Multiply two integers and write them to the screen
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        public void Multiply(int i, int j)
+        {
+            Console.WriteLine(String.Format("Multiply result {0}", i * j));
+        }
+
+        /// <summary>
+        /// Multiply two integers and write them to the screen
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        public void Divide(int i, int j)
+        {
+            if (j == 0)
+                return;
+
+            Console.WriteLine(String.Format("Divide result {0}", i / j));
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathCalculations = new MathCalculations();
+
+            Console.WriteLine("Multi delegate");
+            mathCalculations.Calculations(10, 5);
+
+            Console.WriteLine("");
+            Console.WriteLine("Single delegate");
+            mathCalculations.MultiplyCalculation(10, 3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
